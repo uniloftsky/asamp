624b2e484856fe8c2adba2033eac673fe7d28080
uniloftsky <uniloftsky@gmail.com>
2020-12-09 17:49:16 +0200

    project created

---
+++ src/main/java/uniloftsky/springframework/asamp/AsampApplication.java
@@ -0,0 +1,13 @@
+package uniloftsky.springframework.asamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uniloftsky/springframework/asamp/AsampApplicationTests.java
@@ -0,0 +1,13 @@
+package uniloftsky.springframework.asamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
